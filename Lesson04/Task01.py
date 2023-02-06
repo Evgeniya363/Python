fa1ffe26a771db176e414b72ecc444ccdb546f35
Evgeniya <evavong363@gmail.com>
2023-02-06 19:13:38 +0300

    Modified Task01

--- Lesson04/Task01.py
+++ Lesson04/Task01.py
@@ -1,6 +1,7 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+import math
 
 print(('Вычисление числа пи с помощью ряда Нилаканта:\n'+
        '    π = 3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10) + 4/(10*11*12) - (4/(12*13*14)\n'))
@@ -11,18 +12,19 @@ rounding = len(str(str_d).split('.')[1])
 d = abs(float(str_d))
 
 if 0.1 >= d >= 10**(-10):
-    Pi = i = 3
+    pi = i = 3
     sign = rate = 1
+    PI = math.pi
 
     while rate >=  d:
-        Pi_i = Pi
-        Pi = Pi + sign * 4 / ((i - 1) * i * (i + 1))
-        rate = abs((Pi_i - Pi))
+        pi_i = pi
+        pi = pi + sign * 4 / ((i - 1) * i * (i + 1))
+        rate =  abs(pi-PI)/PI
         i += 2
         sign = - sign
 
-    # print(rate, i)
-    print('π = ', round(Pi, rounding))
+    print(rate, i, rate<d)
+    print('π = ', round(pi, rounding))
 else:
     print('Неверный ввод')
 
